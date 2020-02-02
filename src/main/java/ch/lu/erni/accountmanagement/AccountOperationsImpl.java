f36beb4c05de95af08a6030660692a71aceae7d3
furrerm <markus.furrer@gmail.com>
2020-02-02 17:09:16 +0100

    transactions implemented

--- src/main/java/ch/lu/erni/accountmanagement/AccountOperationsImpl.java
+++ src/main/java/ch/lu/erni/accountmanagement/AccountOperationsImpl.java
@@ -17,7 +17,7 @@ public class AccountOperationsImpl implements AccountOperations{
 
 
     @Override
-    public void deposit(BigDecimal amount) {
+    public synchronized void deposit(BigDecimal amount) {
         BigDecimal currentAmount = account.getCurrentAmount();
         BigDecimal newCurrentAmount = currentAmount.add(amount);
 
@@ -25,7 +25,7 @@ public class AccountOperationsImpl implements AccountOperations{
     }
 
     @Override
-    public void withdraw(BigDecimal amount) {
+    public synchronized void withdraw(BigDecimal amount) {
         BigDecimal currentAmount = account.getCurrentAmount();
         BigDecimal newCurrentAmount = currentAmount.subtract(amount);
 
@@ -33,8 +33,9 @@ public class AccountOperationsImpl implements AccountOperations{
     }
 
     @Override
-    public void transact(BigDecimal amount, Account goalAccount) {
-
+    public synchronized void transact(BigDecimal amount, Account goalAccount) {
+        this.withdraw(amount);
+        new AccountOperationsImpl(goalAccount).deposit(amount);
     }
 
     @Override
--- src/test/java/ch/lu/erni/accountmanagement/AccountOperationsImplTest.java
+++ src/test/java/ch/lu/erni/accountmanagement/AccountOperationsImplTest.java
@@ -57,4 +57,23 @@ public class AccountOperationsImplTest {
 
         Assert.assertEquals(expectedResult, currentAmount);
     }
+
+    @Test
+    public void getCurrentStock_when_Two_Accounts_initialized_and_something_transacted_from_A_to_B_then_return_the_sum_on_B_and_the_subtraction_on_A () {
+
+        Account account_A = new AccountImpl(new BigDecimal(123.45));
+        Account account_B = new AccountImpl(new BigDecimal(1234.56));
+        AccountOperations operations_A = new AccountOperationsImpl(account_A);
+        AccountOperations operations_B = new AccountOperationsImpl(account_B);
+
+
+        operations_A.transact(new BigDecimal(23.45), account_B);
+        BigDecimal currentAmount_A = operations_A.getCurrentStock();
+        BigDecimal currentAmount_B = operations_B.getCurrentStock();
+        BigDecimal expectedResultAccount_A = new BigDecimal(100.00).setScale(2, RoundingMode.HALF_UP);
+        BigDecimal expectedResultAccount_B = new BigDecimal(1258.01).setScale(2, RoundingMode.HALF_UP);
+
+        Assert.assertEquals(expectedResultAccount_A, currentAmount_A);
+        Assert.assertEquals(expectedResultAccount_B, currentAmount_B);
+    }
 }
